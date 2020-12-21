aeedc6044586cd440fcdf29fa95440c1d28df7e1
taran12345 <tarangangil26@gmail.com>
2020-12-21 15:01:24 +0530

    added subtract method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,10 +5,13 @@ public class Calc {
     public static int add(int n1, int n2) {
     	return n1+n2;
     }
+    public static int sub(int n1, int n2) {
+    	return n1-n2;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(add(10,20));
-
+		System.out.println(sub(50,20));
 	}
 
 }

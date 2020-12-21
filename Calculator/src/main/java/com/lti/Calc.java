b81e9224f07f4fb46e8042cab6683de0e8595bba
taran12345 <tarangangil26@gmail.com>
2020-12-21 15:09:25 +0530

    added product method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,10 +8,15 @@ public class Calc {
     public static int sub(int n1, int n2) {
     	return n1-n2;
     }
+    public static int prod(int n1, int n2) {
+    	return n1*n2;
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(add(10,20));
 		System.out.println(sub(50,20));
+		System.out.println(prod(5,20));
 	}
 
 }
